a1b30bffde3ee3c2274caa6c27ca6be111d828fe
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 <sharath.yaragatti@yahoo.com>
2024-08-01 19:17:32 +0530

    changes in query

--- query.js
+++ query.js
@@ -1,6 +1,6 @@
 module.exports = {
-    selectAll : `SELECT * FROM MFX_CUSTOMER1`,
-    selectOne : `SELECT * FROM MFX_CUSTOMER1 WHERE CUST_ID = :id`,
+    selectAll : `select * from mfx_customer1`,
+    selectOne : `select * from mfx_customer1 where cust_id = :id`,
     insert : `insert into mfx_customer1(cust_id,name,email) values(:cust_id,:name,:email)`,
     update : `update mfx_customer1 set name = :name, email = :email, cust_id = :cust_id where cust_id = :id`,
     delete : `delete from mfx_customer1 where cust_id = :id`,
